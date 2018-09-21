50f419ced9f750d6755b0ac2e55be047f14d7a96
Jiefei-Wang <szwjf08@gmail.com>
2018-09-20 22:44:11 -0400

    some comments

--- tests/Unit_test.R
+++ tests/Unit_test.R
@@ -1,5 +1,9 @@
 source("tests\\Support_functions.R")
 
+#k is the dimension of the matrix
+#test_upload:Test the communication between GPU and CPU
+#test_matrixSum: Test the matrix row and column sum and compare the operation times
+
 test_upload(k=100)
 test_matrixSum(k=1000,rowSum=T)
 
